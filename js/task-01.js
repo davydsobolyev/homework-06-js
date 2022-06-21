581c2a9838ab2fd8bc3f2de587420177ad0caf62
Davyd <davydsobolyev@gmail.com>
2022-06-21 15:16:46 +0100

    change

--- js/task-01.js
+++ js/task-01.js
@@ -2,10 +2,10 @@ const items = document.querySelectorAll(".item")
 
 console.log("Number of categories:",items.length)
 
-for(const item of items){
+items.forEach((item) => {
 
     let h2 = item.querySelector("h2");
     console.log("Category:",h2.textContent);
     console.log("Elements;",item.querySelectorAll("li").length)
 
-}
+});
